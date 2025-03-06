b02cb960ff91962f4f3a7b6b724d3a419aa8851a
openorange <2605245956@qq.com>
2025-03-06 09:28:41 +0800

    Initial Commit

---
+++ personnalMusic/personnalMusicApp.swift
@@ -0,0 +1,17 @@
+//
+//  personnalMusicApp.swift
+//  personnalMusic
+//
+//  Created by 胡开成 on 2025/3/6.
+//
+
+import SwiftUI
+
+@main
+struct personnalMusicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ personnalMusicTests/personnalMusicTests.swift
@@ -0,0 +1,17 @@
+//
+//  personnalMusicTests.swift
+//  personnalMusicTests
+//
+//  Created by 胡开成 on 2025/3/6.
+//
+
+import Testing
+@testable import personnalMusic
+
+struct personnalMusicTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
